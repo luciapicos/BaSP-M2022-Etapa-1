74f928ec5f209852798728ec84756fdd8b90c23a
luciapicos <picosgerezluciapaola@gmail.com>
2022-04-19 13:08:23 -0300

    BaSP-W5-2022: JavaScript exercises - 1:Variables and operators

--- semana-05/exercises/01_variables_and_operators.js
+++ semana-05/exercises/01_variables_and_operators.js
@@ -2,8 +2,26 @@
      a) Variables and Operators
         Create two numeric variables and use the sum operator 
         to store the sum value of both numbers in a 3rd variable.*/
+var value1, value2, value3;
+value1=8;
+value2=4;
+value3=value1 + value2;
+console.log(value3);       
     /*b) Create two String variables and concatenate them 
         saving the result in a 3rd variable. */
+var string1, string2, string3;
+string1= "JavaScript exercises: ";
+string2= "Here we go.";
+string3= string1 + string2;
+console.log(string3);      
     /*c) Create two String variables and add the length of each 
         variable (number of letters of the string) 
         saving the result of the sum in a 3rd variable */
+
+var string_a, string_b,quant1, quant2, quant3;
+string_a="abc";
+string_b="defg";
+quant1=string_a.length;
+quant2=string_b.length;
+quant3=string_a.length + string_b.length;
+console.log(quant3);  
